cb4f41c2fb3e745aed0979bf82f739e3e9392008
Bew60000 <anw.nattapon.w@gmail.com>
2024-07-04 13:20:50 +0700

    Header and Backgroung

--- my-project-app/src/App.js
+++ my-project-app/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Background from './img/Background.svg'
 
 function App() {
+
+  const BackgroundImage = {
+    backgroundImage: `url(${Background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-fixed min-w-screen min-h-screen" style={BackgroundImage}>
+      
     </div>
   );
 }
