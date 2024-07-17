6e5ce019084eb515bffff8450b1245d52c5c7c2d
Bew60000 <anw.nattapon.w@gmail.com>
2024-07-17 15:54:42 +0700

    แก้ไข Form 'ลักษณะหลักสูตร' 'กลุ่มเป้าหมาย'

--- my-project-app/src/Components/Form/Basic_Information.js
+++ my-project-app/src/Components/Form/Basic_Information.js
@@ -1,29 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
   FormTextArea,
-  FormSelect,
-  FormRadio,
-  FormInput,
-  FormGroup,
-  FormCheckbox,
-  FormButton,
-  FormField,
-  Form,
-} from 'semantic-ui-react'
+    FormSelect,
+    FormRadio,
+    FormInput,
+    FormGroup,
+    FormCheckbox,
+    FormButton,
+    FormField,
+    Form,
+} from 'semantic-ui-react';
 
 const options = [
   { key: 'm', text: 'Male', value: 'male' },
   { key: 'f', text: 'Female', value: 'female' },
   { key: 'o', text: 'Other', value: 'other' },
-]
+];
 
 class Basic_Information extends Component {
-  state = {}
+  state = {
+    value: '',
+    additionalInfo: '',
+  };
 
-  handleChange = (e, { value }) => this.setState({ value })
+  handleChange = (e, { value }) => this.setState({ value });
+
+  handleAdditionalInfoChange = (e) => this.setState({ additionalInfo: e.target.value });
 
   render() {
-    const { value } = this.state
+    const { value, additionalInfo } = this.state;
     return (
       <div className="grid grid-cols-12 auto-rows-auto gap-3 justify-center p-5">
         <div className='bg-white col-span-8 col-start-3 p-20 border-2 rounded-2xl shadow-10'>
@@ -52,7 +57,7 @@ class Basic_Information extends Component {
               <FormInput fluid label='ปีที่เริ่มดำเนินการเปิดสอน' placeholder='โปรดระบุปีที่เริ่มดำเนินการเปิดสอน' />
             </FormGroup>
 
-            <FormGroup inline>
+            <FormGroup grouped inline>
               <label>ลักษณะของหลักสูตร</label>
               <FormRadio
                 label='เฉพาะสาขาเดียว'
@@ -73,17 +78,29 @@ class Basic_Information extends Component {
                 onChange={this.handleChange}
               />
 
+              {(value === 'md' || value === 'lg') && (
+                <FormGroup widths='equal'>
+                  <FormTextArea
+                    fluid
+                    label='รายละเอียดเพิ่มเติม'
+                    placeholder='โปรดกรอกรายละเอียดเพิ่มเติม'
+                    value={additionalInfo}
+                    onChange={this.handleAdditionalInfoChange}
+                  />
+                </FormGroup>
+              )}
+
             </FormGroup>
+
+
+
             <FormTextArea label='ผลลัพธ์การเรียนรู้ระดับหลักสูตร' placeholder='โปรดอธิบายรายละเอียด' />
-            {/* <FormCheckbox label='I agree to the Terms and Conditions' /> */}
             <FormButton className='grid gap-4 place-items-end'>ต่อไป</FormButton>
           </Form>
         </div>
       </div>
-
-
-    )
+    );
   }
 }
 
-export default Basic_Information
+export default Basic_Information;
--- my-project-app/src/Components/Form/Course_Analysis_Information.js
+++ my-project-app/src/Components/Form/Course_Analysis_Information.js
@@ -1,57 +1,110 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
     FormTextArea,
-    FormSelect,
-    FormRadio,
-    FormInput,
-    FormGroup,
     FormCheckbox,
     FormButton,
-    FormField,
+    FormGroup,
     Form,
-} from 'semantic-ui-react'
+} from 'semantic-ui-react';
 
 class Course_Analysis_Information extends Component {
-    state = {}
+    state = {
+        targetGroups: {
+            highSchool: false,
+            bachelor: false,
+            master: false,
+            doctorate: false,
+            other: false,
+        },
+        otherDetails: '',
+    };
 
-    handleChange = (e, { value }) => this.setState({ value })
+    handleCheckboxChange = (e, { name, checked }) => {
+        this.setState((prevState) => ({
+            targetGroups: {
+                ...prevState.targetGroups,
+                [name]: checked,
+            },
+        }));
+    };
+
+    handleOtherDetailsChange = (e) => {
+        this.setState({ otherDetails: e.target.value });
+    };
 
     render() {
-        const { value } = this.state
+        const { targetGroups, otherDetails } = this.state;
         return (
             <div className="grid grid-cols-12 auto-rows-auto gap-3 justify-center p-5">
                 <div className='bg-white col-span-8 col-start-3 p-20 border-2 rounded-2xl shadow-10'>
                     <Form>
-
                         <FormTextArea fluid label='หลักการและเหตุผลในการขอเปิดหลักสูตร' placeholder='โปรดอธิบายรายละเอียด' />
 
                         <FormGroup grouped inline>
                             <label>กลุ่มเป้าหมายของหลักสูตร หลักสูตรเปิดรับผู้สำเร็จการศึกษาระดับ</label>
-                            {/* <FormField label='This one' control='input' type='checkbox' />
-                            <FormField label='That one' control='input' type='checkbox' /> */}
-                            <FormCheckbox label='มัธยมศึกษา' />
-                            <FormCheckbox label='ปริญญาตรี' />
-                            <FormCheckbox label='ปริญญาโท' />
-                            <FormCheckbox label='ปริญญาเอก' />
-                            <FormCheckbox label='อื่น ๆ' />
+                            <FormCheckbox
+                                label='มัธยมศึกษา'
+                                name='highSchool'
+                                checked={targetGroups.highSchool}
+                                onChange={this.handleCheckboxChange}
+                            />
+                            <FormCheckbox
+                                label='ปริญญาตรี'
+                                name='bachelor'
+                                checked={targetGroups.bachelor}
+                                onChange={this.handleCheckboxChange}
+                            />
+                            <FormCheckbox
+                                label='ปริญญาโท'
+                                name='master'
+                                checked={targetGroups.master}
+                                onChange={this.handleCheckboxChange}
+                            />
+                            <FormCheckbox
+                                label='ปริญญาเอก'
+                                name='doctorate'
+                                checked={targetGroups.doctorate}
+                                onChange={this.handleCheckboxChange}
+                            />
+                            <FormCheckbox
+                                label='อื่น ๆ'
+                                name='other'
+                                checked={targetGroups.other}
+                                onChange={this.handleCheckboxChange}
+                            />
+
+                            {targetGroups.other && (
+                                <FormTextArea
+                                    fluid
+                                    label='โปรดระบุรายละเอียดเพิ่มเติม'
+                                    placeholder='โปรดกรอกรายละเอียดเพิ่มเติม'
+                                    value={otherDetails}
+                                    onChange={this.handleOtherDetailsChange}
+                                />
+                            )}
                         </FormGroup>
 
-                        <FormTextArea fluid label='ผลวิเคราะห์ความต้องการของกลุ่มเป้าหมายใน'
-                            placeholder='วิเคราะห์ความต้องการของกลุ่มเป้าหมายในการเข้าศึกษาหลักสูตรดังกล่าว และระบุข้อมูลที่ใช้ในการคาดการณ์จำนวนผู้เรียนในอนาคต' />
+                        <FormTextArea
+                            fluid
+                            label='ผลวิเคราะห์ความต้องการของกลุ่มเป้าหมายใน'
+                            placeholder='วิเคราะห์ความต้องการของกลุ่มเป้าหมายในการเข้าศึกษาหลักสูตรดังกล่าว และระบุข้อมูลที่ใช้ในการคาดการณ์จำนวนผู้เรียนในอนาคต'
+                        />
 
-
-
-                        <FormTextArea label='ความร่วมมือกับหน่วยงานจากภาคผู้ใช้บัณฑิต' placeholder='โปรดอธิบายรายละเอียด' />
-                        <FormTextArea label='จุดเด่นของหลักสูตรและการดำเนินการที่จะแข่งขันกับหลักสูตรอื่นที่ใกล้เคียง' placeholder='โปรดอธิบายรายละเอียด' />
+                        <FormTextArea
+                            label='ความร่วมมือกับหน่วยงานจากภาคผู้ใช้บัณฑิต'
+                            placeholder='โปรดอธิบายรายละเอียด'
+                        />
+                        <FormTextArea
+                            label='จุดเด่นของหลักสูตรและการดำเนินการที่จะแข่งขันกับหลักสูตรอื่นที่ใกล้เคียง'
+                            placeholder='โปรดอธิบายรายละเอียด'
+                        />
 
                         <FormButton className='grid gap-4 place-items-end'>ต่อไป</FormButton>
                     </Form>
                 </div>
             </div>
-
-
-        )
+        );
     }
 }
 
-export default Course_Analysis_Information
+export default Course_Analysis_Information;
