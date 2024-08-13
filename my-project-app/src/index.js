e89a9273331d63bb1ad49e5b06c22c460428a978
Bew60000 <anw.nattapon.w@gmail.com>
2024-08-14 01:04:51 +0700

    Set Components Role Admin and Index.js

--- my-project-app/src/index.js
+++ my-project-app/src/index.js
@@ -16,8 +16,10 @@ import Management_Information from './Components/RequestForm/Management_Informat
 import Student_Admission from './Components/RequestForm/Student_Admission';
 import Teachers_Information from './Components/RequestForm/Teachers_Information';
 
-import AddUser_Form from './Components/Role_Admin/AddUser_Form';
-import UpdateUser from './Components/Role_Admin/Update_user';
+import AddUser_Form from './Components/Role_Admin/UserManagemaent/AddUser_Form';
+import ShowUserData from './Components/Role_Admin/UserManagemaent/ShowUserData'
+import UserStatistics from './Components/Role_Admin/UserManagemaent/UserStatistics'
+import UpdateUser from './Components/Role_Admin/UserManagemaent/Update_user';
 
 import {
   BrowserRouter,
