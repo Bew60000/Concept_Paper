865c7cc559f52c15baa4adab6cec178312555e3f
fatliqz <asme005a@gmail.com>
2024-10-09 20:37:49 +0700

    demo server

--- server-app/server.js
+++ server-app/server.js
@@ -155,7 +155,9 @@ app.delete('/deletebasic_info/:curriculum_id', async (req, res) => {
 
 app.get('/test/get_info', async (req, res) => {
     try {
-        const result = await pool.query(`select * from basic_infos`);
+        const result = await pool.query(`select * from basic_infos
+join course_analysis_information on basic_infos.curriculum_id = course_analysis_information.curriculum_id  
+join student_admission on basic_infos.curriculum_id = student_admission.curriculum_id`);
         res.json(result.rows);
     } catch (error) {
         console.error(error);
