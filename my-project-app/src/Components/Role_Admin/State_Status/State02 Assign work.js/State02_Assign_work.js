19d829fc09b211222041e391a8742f2ae707ed9a
Bew60000 <anw.nattapon.w@gmail.com>
2024-10-18 21:10:13 +0700

    Radio

--- my-project-app/src/Components/Role_Admin/State_Status/State02 Assign work.js/State02_Assign_work.js
+++ my-project-app/src/Components/Role_Admin/State_Status/State02 Assign work.js/State02_Assign_work.js
@@ -5,7 +5,8 @@ const State02_Assign_work = ({ isOpen, closeModal, selectedForm, studentData, te
 
   const [dataUser, setDataUser] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 4;
+  const [selectedRoles, setSelectedRoles] = useState({}); // เก็บค่าที่เลือกสำหรับแต่ละบุคคล
 
   useEffect(() => {
     axios.get('http://localhost:8080/getinfo_user/all')
@@ -23,27 +24,33 @@ const State02_Assign_work = ({ isOpen, closeModal, selectedForm, studentData, te
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = dataUser.slice(indexOfFirstItem, indexOfLastItem);
 
-
   const totalPages = Math.ceil(dataUser.length / itemsPerPage);
+
+  const handleRoleChange = (userId, role) => {
+    setSelectedRoles(prevRoles => ({
+      ...prevRoles,
+      [userId]: role
+    }));
+  };
 
   if (!isOpen || !selectedForm) return null;
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center">
-      <div className="bg-white p-12 rounded-lg w-11/12 rounded-full min-h-[650px] max-h-[650px] overflow-y-auto mt-16">
+      <div className="bg-white p-12 pt-6 rounded-lg w-11/12 rounded-full min-h-[650px] max-h-[650px]  mt-16">
         {selectedForm && (
           <div className='text-gray-700 px-12'>
 
-            <div className="grid grid-cols-12 gab-1 items-center ">
-              <div className="col-span-12 text-start p-5">
-                <h2 className="font-bold m-0 mb-2 text-blue-900"> การเลือกคณะกรรมการประเมินผล</h2>
+            <div className="grid grid-cols-12 gab-1 items-center mb-3">
+              <div className="col-span-12 text-start p-2">
+                <h2 className="font-bold m-0 text-blue-900"> การเลือกคณะกรรมการประเมินผล</h2>
               </div>
             </div>
 
-            <hr className='border-gray-300 w-11/12 mx-auto' />\
+            {/* <hr className='border-gray-300 w-11/12 mx-auto' />\ */}
 
             <div className='mx-12'>
-              <div className="bg-gray-200 hover:bg-gray-100 p-6 rounded-xl w-full mb-4">
+              <div className="bg-gray-100 p-6 rounded-xl w-full mb-4">
                 <div className="grid grid-cols-12 gap-4 items-center">
                   <div className="col-span-2 text-center">
                     <p className="text-gray-700 font-bold m-1">วันที่ยื่นคำขอ</p>
@@ -51,7 +58,7 @@ const State02_Assign_work = ({ isOpen, closeModal, selectedForm, studentData, te
                   </div>
                   <div className="col-span-3 text-start">
                     <p className="text-blue-800 font-bold m-0">หลักสูตร{selectedForm.majorthai}</p>
-                    <p className='text-gray-500 font-bold m-0'>"{selectedForm.majoreng}"</p>
+                    <p className='text-gray-500 m-0'>{selectedForm.majoreng}</p>
                   </div>
                   <div className="col-span-3 text-start">
                     <p className="text-gray-700 font-bold m-1">คณะ{selectedForm.faculty}</p>
@@ -70,34 +77,62 @@ const State02_Assign_work = ({ isOpen, closeModal, selectedForm, studentData, te
 
         <div className='mx-12 text-gray-700'>
           <hr className='mt-4 border-gray-300 w-11/12 mx-auto mb-4' />
-        
-          <div className="grid grid-cols-12 gab-1 items-center ">
-            <div className="col-span-12 text-start p-5">
-              <h2 className="font-bold m-0 mb-2 text-blue-900"> รายชื่อคณะกรรมการ</h2>
+
+          <div className="grid grid-cols-12 gab-1 items-center mb-3">
+            <div className="col-span-12 text-start ml-12 p-2">
+              <p className="font-bold text-xl m-0 mb-2 text-blue-900"> รายชื่อคณะกรรมการ</p>
             </div>
           </div>
 
-          {currentItems.map((user, index) => (
-            <div key={index} className='mx-12'>
-              <div className="bg-gray-100 p-3 rounded-xl w-full mb-4">
-                <div className="grid grid-cols-12 gap-4 items-center">
-                  <div className="col-span-3 text-start">
-                    <p className="text-gray-800 text-center font-bold m-0">{indexOfFirstItem + index + 1}.{user.name}&nbsp;{user.lastname}</p>
-                  </div>
-                  <div className="col-span-2 text-start">
-                    <p className="text-gray-500">วิทยาเขต: {user.campus}</p>
-                  </div>
-                  <div className="col-span-3 text-start">
-                    <p className="m-1"><strong>โทรศัพท์ :</strong> {user.phone}</p>
-                    <p className="m-1"><strong>Email :</strong> {user.email}</p>
-                  </div>
-                  <div className="col-span-4 text-start">
-                    <p className="m-1">หัวหน้ากรรมการ คณะกรรมการ</p>
+          <div style={{ minHeight: '280px' }}>
+            {currentItems.map((director, index) => (
+              <div key={index} className='mx-12'>
+                <div className="bg-gray-100 hover:bg-gray-200 p-3 rounded-xl w-full mb-2">
+                  <div className="grid grid-cols-12 gap-4 items-center">
+                    <div className="col-span-3 col-start-1 ml-12 mr-2 text-start">
+                      <p className="text-gray-800 font-bold m-0">{indexOfFirstItem + index + 1}. {director.name}&nbsp;{director.lastname}</p>
+                    </div>
+                    <div className="col-span-2 text-start">
+                      <p className="text-gray-500"><strong>วิทยาเขต :</strong> {director.campus}</p>
+                    </div>
+                    <div className="col-span-3 text-start">
+                      <p className="m-1"><strong>โทรศัพท์ :</strong> {director.phone}</p>
+                      <p className="m-1"><strong>Email :</strong> {director.email}</p>
+                    </div>
+                    {/* <div className="col-span-3 text-start">
+                      <p className="m-1">หัวหน้ากรรมการ คณะกรรมการ</p>
+                    </div> */}
+
+                    <div className="col-span-4 text-start">
+                      <label>
+                        <input
+                          type="radio"
+                          name={`role-${director.id}`}
+                          value="หัวหน้าคณะกรรมการ"
+                          checked={selectedRoles[director.id] === 'หัวหน้าคณะกรรมการ'}
+                          onChange={() => handleRoleChange(director.id, 'หัวหน้าคณะกรรมการ')}
+                        />
+                        หัวหน้าคณะกรรมการ
+                      </label>
+
+                      <label className="ml-4">
+                        <input
+                          type="radio"
+                          name={`role-${director.id}`}
+                          value="คณะกรรมการ"
+                          checked={selectedRoles[director.id] === 'คณะกรรมการ'}
+                          onChange={() => handleRoleChange(director.id, 'คณะกรรมการ')}
+                        />
+                        คณะกรรมการ
+                      </label>
+                    </div>
+
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+
+          </div>
 
           {/* Count Page */}
           <div className="flex justify-center mt-4 space-x-2">
@@ -117,7 +152,7 @@ const State02_Assign_work = ({ isOpen, closeModal, selectedForm, studentData, te
           <div className="gap-4 flex justify-center items-center mt-5">
             <button className="bg-blue-500 hover:bg-blue-700 hover:font-bold text-white px-5 py-3 rounded-lg ml-2" type="submit">
               <div className="flex justify-start items-center">
-                ต่อไป
+                ยืนยันการมอบหมายงาน
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 ml-2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                 </svg>
